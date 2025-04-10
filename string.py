9a59bb573dd582573cbd92234a1d744ff4da74f5
manju123-a <adimulammanjunath55@gmail.com>
2025-04-10 11:45:14 +0530

    Add files via upload
    Inital upload of string slicer python code

---
+++ string.py
@@ -0,0 +1,28 @@
+'''str = "hello world"
+print(str[1])
+print(str[-1])
+print(str[1:3])
+print(str[3])
+print(str[1:-1])
+print(str[2:])
+print(str[:-1])
+print(str[::2])
+print(str[1::2])
+print(str[::-1])
+
+#string concetertaion
+n="john"
+p= "Doe"
+m=n + " "+p
+print(m)
+
+#string length
+s1 = "the quick brown fox jumps over the lazy dog.!"
+print(len(s1))'''
+
+ #string manipulation
+s = "hello, world!"
+print(s.upper())
+print(s.lower())
+print(s.strip())
+print(s.replace('o','x'))
